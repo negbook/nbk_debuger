909c090f40f155f273180020d20e284136975e4e
negbook <negbook@gmail.com>
2022-02-25 20:37:05 +0800

    log string ""

--- nativelog.lua
+++ nativelog.lua
@@ -4,9 +4,14 @@ CreateThread(function()
 end)
 RegisterServerEvent("nbk_debuger:writenativelog")
 AddEventHandler("nbk_debuger:writenativelog", function (native,...)
-	local f,err = io.open(GetResourcePath(GetCurrentResourceName())..'/output/'..string.format("%s", native) ..'.log','w+')
+	local f,err = io.open(GetResourcePath(GetCurrentResourceName())..'/output/'..string.format("%s", native) ..'.log','a+')
 	if not f then return print(err) end
 	local opts = {...}
-	f:write(native.."("..table.concat(opts,",")..")")
+	for i,v in pairs(opts) do 
+		if type(v) == 'string' then 
+			opts[i] = '"'..v.. '"'
+		end 
+	end 
+	f:write("\t"..native.."("..table.concat(opts,",")..")\n")
 	f:close()
 end)
